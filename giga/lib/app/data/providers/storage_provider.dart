7257b45ea245fc6762048e2d46464bd5cb801a9e
Anuchit Chalothorn <anoochit@gmail.com>
2023-12-23 20:50:17 +0700

    add comment document

--- giga/lib/app/data/providers/storage_provider.dart
+++ giga/lib/app/data/providers/storage_provider.dart
@@ -3,18 +3,62 @@ import 'dart:typed_data';
 import 'package:appwrite/models.dart';
 import 'package:giga/appwrite.dart';
 
+/// A custom exception thrown when storage-related errors occur.
+class StorageException implements Exception {
+  /// A detailed message describing the error.
+  final String message;
+
+  /// Creates a new [StorageException] with the given [message].
+  StorageException(this.message);
+
+  @override
+  String toString() => 'StorageException: $message';
+}
+
+/// A class that provides access to storage operations.
 class StorageProvider {
-  /// get bucket files
+  /// Retrieves a list of files within the specified bucket.
+  ///
+  /// Throws a [StorageException] if an error occurs during retrieval.
   Future<FileList> getBucketFiles({required String bucketId}) async {
-    return await storage.listFiles(bucketId: bucketId);
+    try {
+      return await storage.listFiles(bucketId: bucketId);
+    } on Exception catch (e) {
+      throw StorageException('Failed to retrieve bucket files: $e');
+    }
   }
 
-  /// get file
+  /// Retrieves the contents of a file as a [Uint8List].
+  ///
+  /// Throws a [StorageException] if an error occurs during retrieval.
   Future<Uint8List> getFile(
       {required String bucketId, required String fileId}) async {
     return await storage.getFileDownload(
       bucketId: bucketId,
       fileId: fileId,
     );
+  }
+
+  /// Retrieves a thumbnail representation of a file.
+  ///
+  /// Throws a [StorageException] if an error occurs during retrieval.
+  getFileThumbnail({
+    required String bucketId,
+    required String fileId,
+    required int width,
+    required int height,
+  }) async {
+    // TODO: Implement thumbnail retrieval logic
+  }
+
+  /// Uploads a file to storage.
+  ///
+  /// Throws a [StorageException] if an error occurs during upload.
+  upload({
+    required String bucketId,
+    required Uint8List fileData,
+    required String filename,
+  }) async {
+    // TODO: Implement upload logic
   }
 }
